ee93ae4cb5467a609abc44b674bccc95ecf6f906
008kevin <kardoskevin07@gmail.com>
2023-06-23 17:00:37 +0200

    Create configuration and instance getter

--- src/main/java/me/kardoskevin07/telecominfo/TelecomInfo.java
+++ src/main/java/me/kardoskevin07/telecominfo/TelecomInfo.java
@@ -2,16 +2,31 @@ package me.kardoskevin07.telecominfo;
 
 import me.kardoskevin07.telecominfo.commands.infoCommand;
 import me.kardoskevin07.telecominfo.commands.listCommand;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TelecomInfo extends JavaPlugin  {
 
+    private static TelecomInfo instance;
+
     @Override
     public void onEnable() {
+        instance = this;
+
+        FileConfiguration config = this.getConfig();
+        saveDefaultConfig();
+
         getLogger().info("Loaded successfully");
+        if (config.getBoolean("debug")) {
+            getLogger().warning("!!! Debug mode is enabled !!!");
+        }
 
         this.getCommand("listCarriers").setExecutor(new listCommand());
         this.getCommand("carrierInfo").setExecutor(new infoCommand());
+    }
+
+    public static TelecomInfo getInstance() {
+        return instance;
     }
 
 
