a59cb68130db63300e3449878f62d7a0cbc77659
008kevin <kardoskevin07@gmail.com>
2024-08-28 23:12:24 +0200

    feat: additonal placeholderAPI placeholders

--- src/main/java/me/kardoskevin07/telecominfo/addons/PapiAddon.java
+++ src/main/java/me/kardoskevin07/telecominfo/addons/PapiAddon.java
@@ -1,16 +1,23 @@
 package me.kardoskevin07.telecominfo.addons;
 
+import com.dbteku.telecom.api.TelecomApi;
+import com.dbteku.telecom.models.Carrier;
+import com.dbteku.telecom.models.WorldLocation;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import me.kardoskevin07.telecominfo.TelecomInfo;
-import org.bukkit.OfflinePlayer;
+import me.kardoskevin07.telecominfo.models.AreaScan;
+import me.kardoskevin07.telecominfo.utils.ParseToMap;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
 
 public class PapiAddon extends PlaceholderExpansion {
 
-    private final TelecomInfo plugin;
-
+    private final FileConfiguration config;
     public PapiAddon(TelecomInfo plugin) {
-        this.plugin = plugin;
+        this.config = plugin.getConfig();
     }
 
     @Override
@@ -29,26 +36,44 @@ public class PapiAddon extends PlaceholderExpansion {
     }
     @Override
     public boolean persist() {return true;}
+    @Override
+    public boolean canRegister() {return true;}
 
-    public String onRequest(OfflinePlayer player, @NotNull String s) {
-        /*if (s.startsWith("carrier_")) {
-            ParseToMap ptm = new ParseToMap();
-
-            String carrierString = s.substring(0, s.lastIndexOf("_") - 1);
-            System.out.println(carrierString);
+    public String onPlaceholderRequest(Player player, @NotNull String s) {
+        if (s.split("_").length >= 2) {
+            String carrierString = s.substring(s.indexOf("_") + 1, s.lastIndexOf("_"));
             Carrier carrier = TelecomApi.get().getCarrierByName(carrierString);
             String dataName = s.substring(s.lastIndexOf("_") + 1);
-            System.out.println(dataName);
 
-            HashMap<String, String> dataMap = ptm.parseCarrier(carrier);
-            if (dataMap.containsKey(dataName)) {
-                return dataMap.get(dataName);
+            if (s.startsWith("carrier_")) {
+                ParseToMap ptm = new ParseToMap();
+
+                HashMap<String, String> dataMap = ptm.parseCarrier(carrier);
+                if (dataMap.containsKey(dataName)) {
+                    return dataMap.get(dataName);
+                }
             }
-        }*/
-        if (s.equalsIgnoreCase("ping")) {
-            return "pong";
-        }
+            if (s.startsWith("location_")) {
+                ParseToMap ptm = new ParseToMap();
 
+                HashMap<String, String> dataMap = ptm.parseCarrierAtLocation(carrier, new WorldLocation(player.getLocation()));
+                if (dataMap.containsKey(dataName)) {
+                    return dataMap.get(dataName);
+                }
+            }
+            if (s.startsWith("area_")) {
+                ParseToMap ptm = new ParseToMap();
+
+                int scanDensity = config.getInt("infoCommand.signal.scanDensity");
+                int scanRadius = config.getInt("infoCommand.signal.scanRadius");
+                AreaScan as = new AreaScan(new WorldLocation(player.getLocation()), carrier, scanRadius, scanDensity);
+
+                HashMap<String, String> dataMap = ptm.parseScan(as);
+                if (dataMap.containsKey(dataName)) {
+                    return dataMap.get(dataName);
+                }
+            }
+        }
         return null;
     }
 }
