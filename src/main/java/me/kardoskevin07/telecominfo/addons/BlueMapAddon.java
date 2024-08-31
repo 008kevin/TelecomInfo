8f9f8032ed369c72a7289e970370642eb7091bdd
008kevin <kardoskevin07@gmail.com>
2024-08-31 14:49:56 +0200

    feat: support multiple worlds

--- src/main/java/me/kardoskevin07/telecominfo/addons/BlueMapAddon.java
+++ src/main/java/me/kardoskevin07/telecominfo/addons/BlueMapAddon.java
@@ -12,55 +12,64 @@ import de.bluecolored.bluemap.api.markers.POIMarker;
 import de.bluecolored.bluemap.api.markers.ShapeMarker;
 import de.bluecolored.bluemap.api.math.Shape;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 public class BlueMapAddon {
     public BlueMapAddon(BlueMapAPI api) {
         List<Carrier> carrierList = TelecomApi.get().getAllCarriers();
         for (Carrier carrier : carrierList) {
-            // Create a MarkerSet for the carrier
-            MarkerSet markerSet = MarkerSet.builder().label(carrier.getName()).build();
+            // Loop through all worlds
+            List<World> worlds = Bukkit.getWorlds();
+            for (World world : worlds) {
+                // Create a MarkerSet for the carrier in world
+                MarkerSet markerSet = MarkerSet.builder().label(carrier.getName()).build();
 
-            Iterator<CellTower> cellTowerIterator = carrier.getTowers();
+                Iterator<CellTower> cellTowerIterator = carrier.getTowers();
+                // loop over all cell towers the carrier has
+                while (cellTowerIterator.hasNext()) {
+                    CellTower cellTower = cellTowerIterator.next();
+                    WorldLocation worldLocation = cellTower.getLocation();
 
-            // loop over all celltowers the carrier has
-            while (cellTowerIterator.hasNext()) {
-                CellTower cellTower = cellTowerIterator.next();
-                WorldLocation worldLocation = cellTower.getLocation();
+                    // Skip iteration if other world
+                    if (!Objects.equals(worldLocation.getWorldName(), world.getName())) {continue;}
 
-                // make a POIMarker for the given cell tower
-                POIMarker poiMarker = POIMarker.builder()
-                        .label(carrier.getName() + " - " + cellTower.getType())
-                        .position(worldLocation.getX()  + 0.5, worldLocation.getY()  + 0.5, worldLocation.getZ()  + 0.5)
-                        .build();
+                    // make a POIMarker for the given cell tower
+                    POIMarker poiMarker = POIMarker.builder()
+                            .label(carrier.getName() + " - " + cellTower.getType())
+                            .position(worldLocation.getX()  + 0.5, worldLocation.getY()  + 0.5, worldLocation.getZ()  + 0.5)
+                            .build();
 
-                // Add poiMarker to the markerSet, with its id as a random UUID
-                markerSet.getMarkers().put(String.valueOf(UUID.randomUUID()), poiMarker);
+                    // Add poiMarker to the markerSet, with its id as a random UUID
+                    markerSet.getMarkers().put(String.valueOf(UUID.randomUUID()), poiMarker);
 
-                int radius = 200;
-                int numOfPoints = (int) (Math.ceil((double) radius / 100) * 16);
+                    int radius = 200;
+                    int numOfPoints = (int) (Math.ceil((double) radius / 100) * 16);
 
-                // build circle shapeMarker for the given cell tower
-                ShapeMarker shapeMarker = ShapeMarker.builder()
-                        .label(carrier.getName() + " - " + cellTower.getType())
-                        .shape(Shape.createCircle(new Vector2d(cellTower.getLocation().getX() + 0.5, cellTower.getLocation().getZ() + 0.5), radius, numOfPoints), (float) (cellTower.getLocation().getY() + 0.5))
-                        .centerPosition()
-                        .maxDistance(Double.MAX_VALUE)
-                        .depthTestEnabled(false)
-                        .build();
+                    // build circle shapeMarker for the given cell tower
+                    ShapeMarker shapeMarker = ShapeMarker.builder()
+                            .label(carrier.getName() + " - " + cellTower.getType())
+                            .shape(Shape.createCircle(new Vector2d(cellTower.getLocation().getX() + 0.5, cellTower.getLocation().getZ() + 0.5), radius, numOfPoints), (float) (cellTower.getLocation().getY() + 0.5))
+                            .centerPosition()
+                            .maxDistance(Double.MAX_VALUE)
+                            .depthTestEnabled(false)
+                            .build();
 
-                // Add shapeMarker to the markerSet, with its id as a random UUID
-                markerSet.getMarkers().put(String.valueOf(UUID.randomUUID()), shapeMarker);
-            }
-            // TODO: make it work on every world, separately
-            api.getWorld(Bukkit.getWorld("world")).ifPresent(world -> {
-                for (BlueMapMap map : world.getMaps()) {
-                    map.getMarkerSets().put(carrier.getName(), markerSet);
+                    // Add shapeMarker to the markerSet, with its id as a random UUID
+                    markerSet.getMarkers().put(String.valueOf(UUID.randomUUID()), shapeMarker);
                 }
-            });
+
+                // Add markers to the given world
+                api.getWorld(Bukkit.getWorld(world.getName())).ifPresent(currentWorld -> {
+                    for (BlueMapMap map : currentWorld.getMaps()) {
+                        map.getMarkerSets().put(carrier.getName(), markerSet);
+                    }
+                });
+            }
         }
     }
 }
