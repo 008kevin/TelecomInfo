f009bf7a4e2bc71e64db689efef2c7ae20b70a3a
008kevin <kardoskevin07@gmail.com>
2024-10-26 19:48:53 +0200

    feat: config options for bluemap

--- src/main/java/me/kardoskevin07/telecominfo/addons/BlueMapAddon.java
+++ src/main/java/me/kardoskevin07/telecominfo/addons/BlueMapAddon.java
@@ -3,6 +3,7 @@ package me.kardoskevin07.telecominfo.addons;
 import com.dbteku.telecom.api.TelecomApi;
 import com.dbteku.telecom.models.Carrier;
 import com.dbteku.telecom.models.CellTower;
+import com.dbteku.telecom.models.TowerType;
 import com.dbteku.telecom.models.WorldLocation;
 import com.flowpowered.math.vector.Vector2d;
 import de.bluecolored.bluemap.api.BlueMapAPI;
@@ -10,16 +11,27 @@ import de.bluecolored.bluemap.api.BlueMapMap;
 import de.bluecolored.bluemap.api.markers.MarkerSet;
 import de.bluecolored.bluemap.api.markers.POIMarker;
 import de.bluecolored.bluemap.api.markers.ShapeMarker;
+import de.bluecolored.bluemap.api.math.Color;
 import de.bluecolored.bluemap.api.math.Shape;
+import me.kardoskevin07.telecominfo.TelecomInfo;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
 
+import java.awt.geom.GeneralPath;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 public class BlueMapAddon {
+
+    private final TelecomInfo mainClass = TelecomInfo.getInstance();
+    private final FileConfiguration config = mainClass.getConfig();
+    private final boolean debug = mainClass.getConfig().getBoolean("debug");
+    private final Logger logger = mainClass.getLogger();
+    
     public BlueMapAddon(BlueMapAPI api) {
         List<Carrier> carrierList = TelecomApi.get().getAllCarriers();
         for (Carrier carrier : carrierList) {
@@ -47,8 +59,11 @@ public class BlueMapAddon {
                     // Add poiMarker to the markerSet, with its id as a random UUID
                     markerSet.getMarkers().put(String.valueOf(UUID.randomUUID()), poiMarker);
 
-                    int radius = 200;
+                    int radius = config.getInt("general." + cellTower.getType() + ".range");
                     int numOfPoints = (int) (Math.ceil((double) radius / 100) * 16);
+                    int color = Integer.decode("0x" + config.getString("general." + cellTower.getType() + ".color"));
+                    
+                    if (debug) {logger.info("Adding tower to bluemap: " + carrier.getName() + ", " + cellTower.getType() + ", " + color);}
 
                     // build circle shapeMarker for the given cell tower
                     ShapeMarker shapeMarker = ShapeMarker.builder()
@@ -57,6 +72,9 @@ public class BlueMapAddon {
                             .centerPosition()
                             .maxDistance(Double.MAX_VALUE)
                             .depthTestEnabled(false)
+                            .fillColor(new Color(color, 0.3f))
+                            .lineColor(new Color(color))
+                            .lineWidth(50)
                             .build();
 
                     // Add shapeMarker to the markerSet, with its id as a random UUID
